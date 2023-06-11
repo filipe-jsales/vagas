250670cf19abcd7c36da75f1099ad7bd6e22b9c6
Filipe Sales <eduardojs999@gmail.com>
2023-06-11 15:07:56 -0300

    feat: mudanças e melhorias no teste3

--- teste3.js
+++ teste3.js
@@ -1,15 +1,23 @@
-var data =  require("./fakeData");
+/**
+ * 
+ * Para tornar o código mais performático, você pode usar o método findIndex para encontrar o índice do usuário a ser excluído. Em seguida, você pode usar o método splice para removê-lo diretamente do array data.
+
+Aqui está o código atualizado:
+ 
+Essa implementação utiliza o método findIndex para localizar o índice do usuário com o nome especificado. Se o índice for diferente de -1, o usuário é encontrado e removido usando o método splice. Caso contrário, é retornado um status 404 com a mensagem "Usuário não encontrado".
+*/
+
+
+const data = require("./fakeData");
 
 module.exports = function(req, res) {
-  
-    var name =  req.query.name;
+  const name = req.query.name;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
-    }
-
+  const index = data.findIndex((user) => user.name === name);
+  if (index !== -1) {
+    data.splice(index, 1);
     res.send("success");
-
+  } else {
+    res.status(404).send("Usuário não encontrado");
+  }
 };
