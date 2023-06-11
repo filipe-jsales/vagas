88ebd1f8a58b43b7067b06a4a36d294b1dc82b55
Filipe Sales <eduardojs999@gmail.com>
2023-06-11 15:04:31 -0300

    feat: mudanças e melhorias no teste1

--- teste1.js
+++ teste1.js
@@ -1,24 +1,35 @@
-var data =  require("./fakeData");
+/**
+ * 
+ * Aqui estão algumas melhorias que podem ser feitas para deixar o código mais legível e eficiente:
 
-const getUser = ( req, res, next ) => {
-    
-    var name =  req.query.name;
+Utilizar a função find em vez de um loop for para buscar o usuário pelo nome. Isso simplifica o código e melhora a legibilidade.
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            res.send(data[i]);
-        }
-    }
+Utilizar a desestruturação para extrair o objeto req diretamente nos parâmetros da função, em vez de acessá-lo por meio do objeto req.
 
+Evitar enviar a resposta dentro de um loop. Após encontrar o usuário desejado, podemos simplesmente retornar o resultado e encerrar a função.
+
+Mover a importação do arquivo fakeData para dentro das funções. Dessa forma, o arquivo só será carregado quando necessário.} req 
+
+ */
+
+const getUser = (req, res) => {
+  const name = req.query.name;
+  const data = require("./fakeData");
+
+  const user = data.find((user) => user.name === name);
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(404).send("Usuário não encontrado");
+  }
 };
 
-const getUsers = ( req, res, next ) => {
-    
-    res.send(data);
-    
+const getUsers = (res) => {
+  const data = require("./fakeData");
+  res.send(data);
 };
 
 module.exports = {
-    getUser,
-    getUsers
+  getUser,
+  getUsers,
 };
