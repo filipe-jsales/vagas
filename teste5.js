1fe89ffe77a5deef0bd743ed3338ad8a18db0ff1
Filipe Sales <eduardojs999@gmail.com>
2023-06-11 15:13:11 -0300

    feat: mudanças e melhorias no teste5

--- teste5.js
+++ teste5.js
@@ -1,9 +1,15 @@
+/**
+ * Para retornar quantas vezes um determinado usuário foi lido no teste1, precisamos modificar o código para buscar o usuário no array data e contar quantas vezes o usuário aparece.
 
+Nessa versão atualizada, utilizamos o método filter para filtrar todos os usuários que têm o mesmo nome especificado. Em seguida, pegamos o comprimento do array resultante para obter a contagem de ocorrências. Essa contagem é então enviada como resposta em uma string formatada.
 
-module.exports = function(req, res){
-    
-    var name =  req.query.name;
+Aqui está o código atualizado:
+ */
 
-    res.send("Usuário " +  name  + "  foi lido 0 vezes.");
+const data = require("./fakeData");
 
+module.exports = function(req, res) {
+  const name = req.query.name;
+  const count = data.filter((user) => user.name === name).length;
+  res.send(`Usuário ${name} foi lido ${count} vezes.`);
 };
